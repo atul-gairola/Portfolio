802d27c1f5b9d8b6bdf28388d03c33a40e76a0ac
atul-gairola <atulgairola08@gmail.com>
2021-09-08 11:58:17 +0530

    feature: implement theme provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ThemeProvider } from "react-jss";
+import { darkTheme } from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
---
+++ src/theme.js
@@ -0,0 +1,6 @@
+export const darkTheme = {
+  colors: {
+    primary: "#fff",
+    background: "#111111",
+  },
+};
