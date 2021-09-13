cad90d63b76f501123388147810cf3b7cfe72ee6
atul-gairola <atulgairola08@gmail.com>
2021-09-13 11:16:07 +0530

    UI: add elements in nav and heroSection

--- src/global/components/Header/components/NavLinks/index.jsx
+++ src/global/components/Header/components/NavLinks/index.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import { createUseStyles } from "react-jss";
 import { Link } from "react-router-dom";
 
-const useStyles = createUseStyles({
+const useStyles = createUseStyles((theme) => ({
   linksContainer: {
     listStyle: "none",
     display: "grid",
     gridTemplateColumns: "auto auto auto",
     gridGap: 30,
     alignItems: "center",
+    "& > a": {
+      fontWeight: "bold",
+      paddingBottom: 5,
+      borderBottom: "2px solid transparent",
+      "&:hover": {
+        borderBottom: `2px solid ${theme.colors.base}`,
+      },
+    },
   },
-});
+}));
 
 const NavLinks = () => {
   const classes = useStyles();
--- src/pages/Home/components/HeroSection/index.jsx
+++ src/pages/Home/components/HeroSection/index.jsx
@@ -2,31 +2,51 @@ import React from "react";
 import { createUseStyles } from "react-jss";
 
 import HeroImage from "../../../../assets/images/HeroImage.png";
+import { ReactComponent as ArrowBottomIcon } from "../../../../assets/images/arrowBottom.svg";
 
 const useStyles = createUseStyles((theme) => ({
   heroSection: {
     display: "grid",
     gridTemplateColumns: "2fr 1fr",
-    height: "calc(100vh - 176px)",
+    gridGap: 100,
+    position: "relative",
+    paddingBottom: "80px",
+    height: "calc(100vh - 175px)",
     alignItems: "center",
+    alignContent: "center",
     "& h1": {
-      fontSize: "4.5em",
+      fontSize: "4em",
     },
     "& h2": {
-      fontSize: "3.5em",
+      fontSize: "3em",
       color: theme.colors.primary,
       marginBottom: 0,
       transform: "translateY(-10px)",
     },
     "& p:first-of-type": {
-      fontSize: "2em",
+      fontSize: "1.7em",
     },
     "& p:last-of-type": {
-      fontSize: "2em",
+      fontSize: "1.8em",
       "& > span": {
         fontWeight: "800",
       },
     },
+    "& button": {
+      color: theme.colors.primary,
+      border: `1px solid ${theme.colors.primary}`,
+      background: "transparent",
+      padding: "10px 20px",
+      fontSize: "1.2rem",
+      cursor: "pointer",
+      marginTop: 30,
+    },
+  },
+  iconContainer: {
+    position: "absolute",
+    left: "50%",
+    transform: "translateX(-50%)",
+    bottom: "0",
   },
 }));
 
@@ -43,9 +63,13 @@ const HeroSection = () => {
           From <span>personal websites</span> to <span>complex webapps</span>{" "}
           and everything in between
         </p>
+        <button>Contact me</button>
       </div>
       <div>
         <img src={HeroImage} alt="Atul Gairola" />
+      </div>
+      <div className={classes.iconContainer}>
+        <ArrowBottomIcon />
       </div>
     </section>
   );
